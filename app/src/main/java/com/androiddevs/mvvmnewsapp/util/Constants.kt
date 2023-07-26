6446de1d4fed25084b0038d750b483915ffde3cb
Yash Ginoya <yashginoya2003@gmail.com>
2023-07-26 21:24:00 +0530

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+//        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = "fca8dd433d3b45ca8b1648f52a0ff3fe"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
